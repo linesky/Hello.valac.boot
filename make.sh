635cd1af3a5a98ee0275f72e103e6586a3f383da
sky line <32512284+linesky@users.noreply.github.com>
2024-04-28 19:29:10 +0100

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,9 @@
+printf "\x1bc\x1b[43;37m give me a .c file to compile ? "
+read ai
+nasm -o /tmp/boot.bin boot.asm
+valac -c "$ai" -o /tmp/kernel.o 
+ld -T link.ld /tmp/kernel.o -o /tmp/hello.com -nostdlib
+objcopy -O binary  /tmp/hello.com  /tmp/hellos.com
+dd if=/tmp/hellos.com of=/tmp/boot.bin seek=1 conv=notrunc
+cp -f /tmp/boot.bin ./
+/usr/bin/qemu-system-x86_64 -boot a -fda boot.bin
